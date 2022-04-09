b4162fa2fe8d94d7280da81b52294384ba59754e
pangyuteng <pangyuteng@gmail.com>
2022-04-09 12:07:47 -0700

    wip convert_imagenet_to_records.py

--- prepare/convert_imagenet_to_records.py
+++ prepare/convert_imagenet_to_records.py
@@ -48,7 +48,7 @@ def main(argv):
 
     writer = tf.python_io.TFRecordWriter(outfile)
     for f in tqdm(files):
-        print(i)
+        #print(i)
         image = get_image(f, IMSIZE, is_crop=True, resize_w=IMSIZE)
         image = colorize(image)
         assert image.shape == (IMSIZE, IMSIZE, 3)
